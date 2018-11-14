729f33dbd97ebe1ab43da7b5a74f467460b9c4d3
Lei Hu <hulei123@outlook.com>
2018-11-14 11:22:07 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,2 +1,2 @@
 # example
-python src/main.py --channel_rito 0.25 --optimizer adam --epoch 200 --num_classes 5 --batch_size 128 --num_samples 256133 --num_valid_samples 12095 --is_train 1 --dataset_dir /media/data1/hl/expression_cls_data/expression_dect_tfrecords64x64
+python3 src/main.py --channel_rito 0.25 --optimizer adam --epoch 200 --num_classes 5 --batch_size 128 --num_samples 256133 --num_valid_samples 12095 --is_train 1 --dataset_dir /media/data1/hl/expression_cls_data/expression_dect_tfrecords64x64
